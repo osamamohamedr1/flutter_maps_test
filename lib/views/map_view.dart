ed1ebe915d68a54083c58465a39c6cd6f9a139db
Osama Mohamed <osamarizk262@gmail.com>
2025-06-28 00:11:37 +0300

    add map controller and use camera update class

--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -19,16 +19,47 @@ class _MapViewState extends State<MapView> {
     super.initState();
   }
 
+  late GoogleMapController mapController;
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      cameraTargetBounds: CameraTargetBounds(
-        LatLngBounds(
-          southwest: LatLng(31.1860747481394, 31.4676724649157),
-          northeast: LatLng(31.28735385986459, 31.605479137667256),
+    return Stack(
+      children: [
+        GoogleMap(
+          onMapCreated: (GoogleMapController controller) {
+            mapController = controller;
+          },
+          // cameraTargetBounds: CameraTargetBounds(
+          //   LatLngBounds(
+          //     southwest: LatLng(31.1860747481394, 31.4676724649157),
+          //     northeast: LatLng(31.28735385986459, 31.605479137667256),
+          //   ),
+          // ),
+          initialCameraPosition: initialCameraPosition,
         ),
-      ),
-      initialCameraPosition: initialCameraPosition,
+
+        Positioned(
+          bottom: 20,
+          right: 20,
+          left: 20,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            onPressed: () {
+              mapController.animateCamera(
+                CameraUpdate.newLatLng(
+                  LatLng(31.0361588230587, 30.92684072019934),
+                ),
+              );
+            },
+            child: const Text('Locate Me'),
+          ),
+        ),
+      ],
     );
   }
 }
