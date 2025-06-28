b710c2d72520c29d6bb21ca017d38c8bdee7a751
Osama Mohamed <osamarizk262@gmail.com>
2025-06-28 15:00:52 +0300

    add marker

--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -16,17 +16,21 @@ class _MapViewState extends State<MapView> {
       zoom: 10,
       target: LatLng(31.254757038716278, 31.543353972025066),
     );
+    initMarkers();
     super.initState();
   }
 
   late GoogleMapController mapController;
+  Set<Marker> markers = {};
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         GoogleMap(
+          markers: markers,
           onMapCreated: (GoogleMapController controller) {
             mapController = controller;
+            intitalizeMapStyle();
           },
           // cameraTargetBounds: CameraTargetBounds(
           //   LatLngBounds(
@@ -37,29 +41,45 @@ class _MapViewState extends State<MapView> {
           initialCameraPosition: initialCameraPosition,
         ),
 
-        Positioned(
-          bottom: 20,
-          right: 20,
-          left: 20,
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-            onPressed: () {
-              mapController.animateCamera(
-                CameraUpdate.newLatLng(
-                  LatLng(31.0361588230587, 30.92684072019934),
-                ),
-              );
-            },
-            child: const Text('Locate Me'),
-          ),
-        ),
+        // Positioned(
+        //   bottom: 20,
+        //   right: 20,
+        //   left: 20,
+        //   child: ElevatedButton(
+        //     style: ElevatedButton.styleFrom(
+        //       backgroundColor: Colors.blue,
+        //       foregroundColor: Colors.white,
+        //       shape: RoundedRectangleBorder(
+        //         borderRadius: BorderRadius.circular(10),
+        //       ),
+        //     ),
+        //     onPressed: () {
+        //       mapController.animateCamera(
+        //         CameraUpdate.newLatLng(
+        //           LatLng(31.0361588230587, 30.92684072019934),
+        //         ),
+        //       );
+        //     },
+        //     child: const Text('Locate Me'),
+        //   ),
+        // ),
       ],
     );
+  }
+
+  intitalizeMapStyle() async {
+    var nightMap = await DefaultAssetBundle.of(
+      context,
+    ).loadString('assets/map_styles/night_map_style.json');
+
+    mapController.setMapStyle(nightMap);
+  }
+
+  void initMarkers() {
+    var myMarker = Marker(
+      markerId: MarkerId('1'),
+      position: LatLng(31.254757038716278, 31.543353972025066),
+    );
+    markers.add(myMarker);
   }
 }
